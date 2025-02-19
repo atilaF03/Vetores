03d2eb957f6c461c5e4de2bf942fbe20fbec326d
Átila <144128328+atilaF03@users.noreply.github.com>
2025-02-19 18:24:49 -0300

    Create Vetores2.java

---
+++ Vetores2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Vetores2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maior =0;
+        int[] numeros = new int[5];
+
+      for (int i : numeros) {
+       System.out.println("o valor é: "+ numeros[i]); 
+
+
+       
+       sc.close(); 
+      }  
+    }
+}
