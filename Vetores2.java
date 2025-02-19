ebbde721b706acb3745a7cefc4288b5b178439d6
Átila <144128328+atilaF03@users.noreply.github.com>
2025-02-19 21:58:29 +0000

    atualizar

--- Vetores2.java
+++ Vetores2.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 
 public class Vetores2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int maior =0;
-        int[] numeros = new int[5];
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] numeros = new int[5];
+    int maior = numeros[0];
 
-      for (int i : numeros) {
-       System.out.println("o valor é: "+ numeros[i]); 
+    System.out.println(" digite 10 numero inteiros");
+    for (int i = 0; i < numeros.length; i++) {
+      System.out.println("digite o numero" + (i++) + ": ");
+    }
 
+   for (int i <0; i< numeros.length; i++){
+    if (numeros[i]>  maior) 
+    {
+    maior = numeros[i];
+    }
+   }
+System.out.println("o maior valor é" + maior);
+   
 
-       
-       sc.close(); 
-      }  
+      sc.close();
     }
 }
